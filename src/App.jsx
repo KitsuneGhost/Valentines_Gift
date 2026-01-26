af4947e2152e5dcdea69b82f9da1cf08c618e73f
KitsuneGhost <3722146@gmail.com>
2026-01-26 18:41:54 +0100

    ivisibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
+    const [revealed, setRevealed] = useState(false);
+
     return (
         <div>
             <h1>Hi Ari ❤️</h1>
-            <p>This page is for you.</p>
+
+            <button onClick={() => setRevealed(true)}>
+                Click me
+            </button>
+
+            {revealed && <p>I love you.</p>}
         </div>
     );
 }
